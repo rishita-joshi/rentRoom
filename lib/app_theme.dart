f30ac7281dc920feb196c56aa7575ec509fb0902
Umesh Chauhan <Umesh>
2023-05-31 08:53:01 +0530

    package update

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -1,51 +1,50 @@
-
 import 'package:flutter/material.dart';
-import 'package:rent_a_room/font_asset.dart';
 import 'package:rent_a_room/themes/myColors.dart';
 
 abstract class AppTheme {
   static ThemeData get lightTheme => ThemeData(
-
-    colorScheme: ColorScheme(brightness: Brightness.light,
-
-     primary: ColorPalette.darkGrayColor, onPrimary: ColorPalette.whiteColor, 
-     secondary: ColorPalette.lightBlueColor , onSecondary: ColorPalette.textColorGray, error: ColorPalette.redColor, onError: ColorPalette.redColor,
-      background: ColorPalette.redColor, onBackground: ColorPalette.textColorGray, surface: ColorPalette.darkGrayColor, onSurface:  ColorPalette.darkGrayColor),
-       
-    
-        scaffoldBackgroundColor: ColorPalette.whiteColor,
-
-appBarTheme: AppBarTheme(backgroundColor: ColorPalette.whiteColor ,
-),    
-iconTheme: IconThemeData(color: ColorPalette.darkGrayColor),    
-iconButtonTheme: IconButtonThemeData(style: ButtonStyle(iconColor:  MaterialStateProperty.all(ColorPalette.blackColor) ,  iconSize: MaterialStatePropertyAll(15) )),
-cardColor: Colors.white,
-buttonTheme: ButtonThemeData(buttonColor: ColorPalette.darkGrayColor , 
-shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(10.0)),),
-       elevatedButtonTheme: ElevatedButtonThemeData(style: ButtonStyle(
-elevation: MaterialStatePropertyAll(10.0),
-backgroundColor:MaterialStatePropertyAll(ColorPalette.darkGrayColor
-),
-minimumSize: MaterialStateProperty.all(Size(170.0, 50.0)),
+      colorScheme: ColorScheme(
+          brightness: Brightness.light,
+          primary: ColorPalette.darkGrayColor,
+          onPrimary: ColorPalette.whiteColor,
+          secondary: ColorPalette.lightBlueColor,
+          onSecondary: ColorPalette.textColorGray,
+          error: ColorPalette.redColor,
+          onError: ColorPalette.redColor,
+          background: ColorPalette.redColor,
+          onBackground: ColorPalette.textColorGray,
+          surface: ColorPalette.darkGrayColor,
+          onSurface: ColorPalette.darkGrayColor),
+      scaffoldBackgroundColor: ColorPalette.whiteColor,
+      appBarTheme: AppBarTheme(
+        backgroundColor: ColorPalette.whiteColor,
+      ),
+      iconTheme: IconThemeData(color: ColorPalette.darkGrayColor),
+      iconButtonTheme: IconButtonThemeData(
+          style: ButtonStyle(
+              iconColor: MaterialStateProperty.all(ColorPalette.blackColor),
+              iconSize: MaterialStatePropertyAll(15))),
+      cardColor: Colors.white,
+      buttonTheme: ButtonThemeData(
+        buttonColor: ColorPalette.darkGrayColor,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+        elevation: MaterialStatePropertyAll(10.0),
+        backgroundColor: MaterialStatePropertyAll(ColorPalette.darkGrayColor),
+        minimumSize: MaterialStateProperty.all(Size(170.0, 50.0)),
 //alignment: Alignment.bottomCenter,
-
-
-       ))
-       
-       
-       );
-
-      
+      )));
 
   static ThemeData get darkTheme => ThemeData(
-        
         scaffoldBackgroundColor: ColorPalette.darkGrayColor,
-
-        appBarTheme: AppBarTheme(backgroundColor: ColorPalette.darkGrayColor ,
-),
-iconTheme: IconThemeData(color: ColorPalette.whiteColor),   
-cardColor: Colors.amber,
-        
+        appBarTheme: AppBarTheme(
+          backgroundColor: ColorPalette.darkGrayColor,
+        ),
+        iconTheme: IconThemeData(color: ColorPalette.whiteColor),
+        cardColor: Colors.amber,
       );
 } // textTheme: ThemeData.light().textTheme.copyWith(
         //       displayLarge: const TextStyle(
